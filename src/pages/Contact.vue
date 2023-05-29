9fed9e30bdf69871da2dcb4dbd00bf8abe885d67
Pascal Pham <pham.anhkhoapascal@gmail.com>
2023-05-29 15:33:52 +0900

    Contact: send btn behavior

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -9,7 +9,8 @@
         <p class="text-[16px] text-[#7C7C7C] underline">17 Đường Số 16, Thảo Điền, Quận 2,<br />
           Thành phố Hồ Chí Minh, <br />
           Viêt Nam
-          <a href="https://maps.google.com/" class="text-blue-500">(View on map)</a></p>
+          <a href="https://maps.google.com/" class="text-blue-500">(View on map)</a>
+        </p>
       </div>
       <div class="ml-28">
         <p class="text-[24px] pb-8">Opening Hours</p>
@@ -41,7 +42,8 @@
       <div v-for="field in fields" class="grid grid-cols-1 gap-3 pb-8">
         <label class="text-[16px]">{{ field.title }}</label>
         <label class="text-[16px] text-[#7C7C7C]">{{ field.placeholder }}</label>
-        <input type="text" class="w-full border-0 border-b-2 border-gray-200 p-0 focus:ring-gray-100 active:ring-gray-100">
+        <input type="text" v-model="dataForm[field.key]"
+          class="w-full border-0 border-b-2 border-gray-200 p-0 focus:ring-gray-100 active:ring-gray-100">
       </div>
 
       <div class="my-8 grid grid-cols-2">
@@ -51,12 +53,10 @@
             <option>Parents&Kids (2-3yo)</option>
           </select>
         </div>
-        <button class="btn-secondary justify-self-end w-56">
+        <button class="btn-secondary justify-self-end w-56" :disabled=disabled>
           Send
         </button>
       </div>
-
-      <!-- <input type="button" value="Submit"> -->
     </form>
 
   </div>
@@ -74,15 +74,21 @@ export default {
   data() {
     return {
       fields: [
-        { title: 'Your Name', placeholder: 'e.g, Benjamin' },
-        { title: 'Your Surname', placeholder: 'e.g, Nguyen' },
-        { title: 'Your Date of Birth', placeholder: 'dd/mm/yyyy' },
-        { title: 'Your Email', placeholder: 'mail@ssjc.com' },
-        { title: 'Your Phone Number', placeholder: '+84 02xx xxx xxxx' },
-      ]
+        { title: 'Your Name', placeholder: 'e.g, Benjamin', key: 'name' },
+        { title: 'Your Surname', placeholder: 'e.g, Nguyen', key: 'surname' },
+        { title: 'Your Date of Birth', placeholder: 'dd/mm/yyyy', key: 'dateOfBirth' },
+        { title: 'Your Email', placeholder: 'mail@ssjc.com', key: 'email' },
+        { title: 'Your Phone Number', placeholder: '+84 02xx xxx xxxx', key: 'phoneNumber' },
+      ],
+      dataForm: {
+        'name': '', 'surname': '', 'dateOfBirth': '', 'email': '', 'phoneNumber': ''
+      }
     }
   },
-  methods: {
+  computed: {
+    disabled() {
+      return !Object.keys(this.dataForm).every((k) => this.dataForm[k] !== '')
+    }
   }
 }
 </script>
