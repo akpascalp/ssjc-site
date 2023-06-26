555b58e675ac393d8a0dd42edb53b0e901792593
Pascal Pham <pham.anhkhoapascal@gmail.com>
2023-06-26 10:56:55 +0900

    Fix Sidebar chevron btn

--- src/layouts/partials/Sidebar.vue
+++ src/layouts/partials/Sidebar.vue
@@ -34,15 +34,19 @@
                 <button v-else class="text-[16px] text-black text-left flex relative" @click="showMenu = false">
                   <div class="transition ease-in-out duration-100" @mouseover="displayedSubIndex = null"
                     :class="(displayedSubIndex !== null && displayedIndex !== index) ? 'opacity-25' : 'opacity-100'">
-                    <router-link v-if="menu.to !== undefined" :to="menu.to !== undefined ? menu.to : ''">{{ menu.name
-                    }}</router-link>
+                    <router-link v-if="menu.to !== undefined" :to="menu.to !== undefined ? menu.to : ''">
+                      {{ menu.name }}
+                    </router-link>
                     <button v-else
                       @click="displayedSubIndex === index ? displayedSubIndex = null : displayedSubIndex = index"
-                      @click.stop>{{ menu.name }}</button>
+                      @click.stop>{{ menu.name }}
+                    </button>
                   </div>
                   <svg v-if="menu.subMenu" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                     stroke-width="1" stroke="currentColor" class="w-4 h-4 ml-3 mt-1 transition duration-150 ease-in-out "
-                    :class="displayedSubIndex == index ? 'transform -rotate-180' : ''">
+                    :class="displayedSubIndex == index ? 'transform -rotate-180' : ''"
+                    @click="displayedSubIndex === index ? displayedSubIndex = null : displayedSubIndex = index"
+                    @click.stop>
                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                   </svg>
                   <TransitionRoot as="template" enter="transition ease-in-out duration-300"
@@ -59,7 +63,7 @@
               </div>
             </div>
           </div>
-          <img src="/img_menu.png" class="pr-20 bg-contain bg-center"/>
+          <img src="/img_menu.png" class="pr-20 bg-contain bg-center" />
         </div>
       </TransitionChild>
 
