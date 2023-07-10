c7120321b3d3876f85ad8baf7c29638a1a5af89a
Pascal Pham <pham.anhkhoapascal@gmail.com>
2023-07-10 08:24:15 +0200

    set max-width to 1800px

--- src/components/CallToAction.vue
+++ src/components/CallToAction.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="grid m-2 sm:h-96 content-start justify-items-center bg-[url('/img_6.png')] sm:bg-contain bg-center bg-no-repeat text-white">
-    <p class="relative pt-2 m-2 sm:w-5/12 md:w-3/4 lg:w-5/12 text-[24px] sm:pt-24 sm:pb-12 sm:-ml-24 min-[2130px]:w-1/3 min-[2130px]:-ml-12">
+    <p class="relative pt-2 m-2 sm:w-5/12 md:w-3/4 lg:w-5/12 text-[24px] sm:pt-24 sm:pb-12 sm:-ml-24">
       {{ text }}
       <button class="sm:absolute sm:-bottom-16 sm:left-0 btn-secondary mb-2">
         {{ btnText }}
--- src/layouts/LayoutDefault.vue
+++ src/layouts/LayoutDefault.vue
@@ -1,16 +1,16 @@
 <template>
+  <div class="flex flex-col">
     <div class="flex flex-col">
-        <div class="flex flex-col">
-            <SiteSidebar />
-            <main class="sm:pl-[120px] z-0 sm:overflow-x-hidden">
-                <router-view />
-            </main>
-        </div>
-        <SiteFooter class="sm:pl-[120px]" />
+      <SiteSidebar />
+      <main class="sm:pl-[120px] z-0 sm:overflow-x-hidden">
+        <router-view />
+      </main>
     </div>
+    <SiteFooter class="sm:pl-[120px]" />
+  </div>
 </template>
 
 <script setup>
-    import SiteFooter from '@/layouts/partials/Footer.vue'
-    import SiteSidebar from '@/layouts/partials/Sidebar.vue'
+import SiteFooter from '@/layouts/partials/Footer.vue'
+import SiteSidebar from '@/layouts/partials/Sidebar.vue'
 </script>
