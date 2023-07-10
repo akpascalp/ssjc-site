2b4bd729b96a2b033d689c0f46456383e578cbfd
Pascal Pham <pham.anhkhoapascal@gmail.com>
2023-07-10 08:50:12 +0200

    Bordure sidebar

--- src/layouts/partials/Sidebar.vue
+++ src/layouts/partials/Sidebar.vue
@@ -24,46 +24,49 @@
       <TransitionChild as="template" enter="transition ease-in-out duration-700" enter-from="-translate-x-60 opacity-50"
         enter-to="translate-x-0" leave="transition ease-in-out duration-700" leave-from="translate-x-0"
         leave-to="-translate-x-60 opacity-0">
-        <div class="grid grid-cols-2 bg-white">
-          <div class="relative flex flex-col flex-1">
-            <div class="flex flex-col h-screen inset-y-0 ml-14 my-24 sm:ml-24">
-              <div v-for="(menu, index) in menus" class="group" @mouseover="displayedIndex = index">
-                <div v-if="menu.name === '_break'">
-                  <div class="m-56" />
-                </div>
-                <button v-else class="text-[16px] text-black text-left flex relative my-3" @click="showMenu = false">
-                  <div class="transition ease-in-out duration-100" @mouseover="displayedSubIndex = null"
-                    :class="(displayedSubIndex !== null && displayedIndex !== index) ? 'opacity-25' : 'opacity-100'">
-                    <router-link v-if="menu.to !== undefined" :to="menu.to !== undefined ? menu.to : ''">
-                      {{ menu.name }}
-                    </router-link>
-                    <button v-else
-                      @click="displayedSubIndex === index ? displayedSubIndex = null : displayedSubIndex = index"
-                      @click.stop>{{ menu.name }}
-                    </button>
+        <div class="flex">
+          <div class="w-[1px] h-screen bg-black/[.6]"></div>
+          <div class="grid grid-cols-2 bg-white">
+            <div class="relative flex flex-col flex-1">
+              <div class="flex flex-col h-screen inset-y-0 ml-14 my-24 sm:ml-24">
+                <div v-for="(menu, index) in menus" class="group" @mouseover="displayedIndex = index">
+                  <div v-if="menu.name === '_break'">
+                    <div class="m-56" />
                   </div>
-                  <svg v-if="menu.subMenu" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                    stroke-width="1" stroke="currentColor" class="w-4 h-4 ml-3 mt-1 transition duration-150 ease-in-out "
-                    :class="displayedSubIndex == index ? 'transform -rotate-180' : ''"
-                    @click="displayedSubIndex === index ? displayedSubIndex = null : displayedSubIndex = index"
-                    @click.stop>
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                  </svg>
-                  <TransitionRoot as="template" enter="transition ease-in-out duration-300"
-                    enter-from="-translate-x-20 opacity-0" enter-to="translate-x-0 opacity-100"
-                    leave="transition ease-in-out duration-300" leave-from="translate-x-0 opacity-100"
-                    leave-to="-translate-x-20 opacity-0" :show="displayedSubIndex === index">
-                    <div class="absolute ml-48 -mt-3 lg:ml-72 md:ml-48">
-                      <button v-for="subMenu in menu.subMenu" class="text-[16px] w-56 text-left my-3">
-                        <router-link :to="subMenu.to">{{ subMenu.name }}</router-link>
+                  <button v-else class="text-[16px] text-black text-left flex relative my-3" @click="showMenu = false">
+                    <div class="transition ease-in-out duration-100" @mouseover="displayedSubIndex = null"
+                      :class="(displayedSubIndex !== null && displayedIndex !== index) ? 'opacity-25' : 'opacity-100'">
+                      <router-link v-if="menu.to !== undefined" :to="menu.to !== undefined ? menu.to : ''">
+                        {{ menu.name }}
+                      </router-link>
+                      <button v-else
+                        @click="displayedSubIndex === index ? displayedSubIndex = null : displayedSubIndex = index"
+                        @click.stop>{{ menu.name }}
                       </button>
                     </div>
-                  </TransitionRoot>
-                </button>
+                    <svg v-if="menu.subMenu" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                      stroke-width="1" stroke="currentColor" class="w-4 h-4 ml-3 mt-1 transition duration-150 ease-in-out "
+                      :class="displayedSubIndex == index ? 'transform -rotate-180' : ''"
+                      @click="displayedSubIndex === index ? displayedSubIndex = null : displayedSubIndex = index"
+                      @click.stop>
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                    </svg>
+                    <TransitionRoot as="template" enter="transition ease-in-out duration-300"
+                      enter-from="-translate-x-20 opacity-0" enter-to="translate-x-0 opacity-100"
+                      leave="transition ease-in-out duration-300" leave-from="translate-x-0 opacity-100"
+                      leave-to="-translate-x-20 opacity-0" :show="displayedSubIndex === index">
+                      <div class="absolute ml-48 -mt-3 lg:ml-72 md:ml-48">
+                        <button v-for="subMenu in menu.subMenu" class="text-[16px] w-56 text-left my-3">
+                          <router-link :to="subMenu.to">{{ subMenu.name }}</router-link>
+                        </button>
+                      </div>
+                    </TransitionRoot>
+                  </button>
+                </div>
               </div>
             </div>
+            <img src="/img_menu.png" class="hidden sm:block pr-20 bg-contain bg-center" />
           </div>
-          <img src="/img_menu.png" class="hidden sm:block pr-20 bg-contain bg-center" />
         </div>
       </TransitionChild>
 
@@ -105,7 +108,7 @@ export default {
           'to': '/interval-training'
         }]
       }, {
-        'name': 'Social',
+        'name': 'Social',  // TODO not visible on paysage mode mobile
         'to': '/social',
       }, {
         'name': 'Working Space',
